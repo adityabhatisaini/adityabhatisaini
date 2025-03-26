fed6a84e68dceef90f71a493f5add31ac8a165f6
Aditya Bhati <163522402+adityabhatisaini@users.noreply.github.com>
2025-03-26 13:16:45 +0530

    v12

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ menuIcon.onclick = () => {
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getMessaging } from "firebase/messaging";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -44,7 +45,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const messaging = getMessaging(app);
 const analytics = getAnalytics(app);
 
 // Request Notification Permission
